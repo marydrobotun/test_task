952ff80311a6bcb72fb8f99d7157c0ab1c1193a8
marydrobotun <marydrobotun@gmail.com>
2023-04-28 11:56:39 +0300

    settings changed: previous attempt doesnt work

--- common/settings.py
+++ common/settings.py
@@ -1,6 +1,3 @@
-from os.path import dirname
-import sys
 
-project_path = dirname(dirname(sys.modules['__main__'].__file__))
-dwh_connect_string = f'sqlite:///{project_path}/dwh.sqlite'
-source_connect_string = f'sqlite:///{project_path}/database.sqlite'
+dwh_connect_string = f'sqlite:////home/mary/my_projects/skyeng_test/dwh.sqlite'
+source_connect_string = f'sqlite:////home/mary/my_projects/skyeng_test/database.sqlite'
